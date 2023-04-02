048bafa0dc87e02995f1b4e3febda833e1e7c03c
ishyj <thgpddl@163.com>
2023-04-02 23:59:49 +0800

    Initial commit

---
+++ create_json.py
@@ -0,0 +1,19 @@
+import json
+from os.path import join
+import glob
+
+
+if __name__ == '__main__':
+    # path to folder that contains images
+    img_folder = r'dataset\ShanghaiTech_Crowd_Counting_Dataset\part_B_final\test_data\images'
+
+    # path to the final json file
+    output_json = 'test_data.json'
+
+    img_list = []
+
+    for img_path in glob.glob(join(img_folder,'*.jpg')):
+        img_list.append(img_path)
+
+    with open(output_json,'w') as f:
+        json.dump(img_list,f)
