9012df88b5f838a95d76edacf1330964d28bf374
Huoyuanjia <thgpddl@163.com>
2023-04-04 15:57:43 +0800

    优化cerate_json

--- create_json.py
+++ create_json.py
@@ -5,15 +5,22 @@ import glob
 
 if __name__ == '__main__':
     # path to folder that contains images
-    img_folder = r'dataset\ShanghaiTech_Crowd_Counting_Dataset\part_B_final\test_data\images'
+    train_img_folder = r'dataset\ShanghaiTech_Crowd_Counting_Dataset\part_B_final\train_data\images'
+    train_output_json = 'train_data.json'
 
-    # path to the final json file
-    output_json = 'test_data.json'
+    test_img_folder = r'dataset\ShanghaiTech_Crowd_Counting_Dataset\part_B_final\test_data\images'
+    test_output_json = 'test_data.json'
 
     img_list = []
 
-    for img_path in glob.glob(join(img_folder,'*.jpg')):
+    for img_path in glob.glob(join(train_img_folder,'*.jpg')):
         img_list.append(img_path)
 
-    with open(output_json,'w') as f:
+    with open(train_output_json,'w') as f:
+        json.dump(img_list,f)
+
+    for img_path in glob.glob(join(test_img_folder,'*.jpg')):
+        img_list.append(img_path)
+
+    with open(test_output_json,'w') as f:
         json.dump(img_list,f)
